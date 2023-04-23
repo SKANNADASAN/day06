a5fa04b053c452c383359791b44d72fb48d15848
SKANNADASAN <124970082+SKANNADASAN@users.noreply.github.com>
2023-04-23 05:38:53 +0530

    Add files via upload

---
+++ day06-3.js
@@ -0,0 +1,25 @@
+class Person{
+    constructor(name,heigth,weight,age,colour,qualification,city, state){
+        this.name=name;
+        this.heigth=heigth;
+        this.weigth=weigth;
+        this.age=age;
+        this.colour=colour;
+        this.qualification=qualification;
+        this.city=city;
+        this.state=state;
+    }
+    details(){
+        console.log(`
+        name:${this.name}
+        height:${this.heigth}
+        weight:${this.weigth}
+        age:${this.age}
+        color:${this.colour}
+        qualification:${this.qualification}
+        city:${this.city}
+        state:${this.state}`)
+    }
+}
+let Person1=new Person("Kannadasan","5.3","50","26","Saffron","BE","CHENNAI","TamilNadu")
+Person1.details()
